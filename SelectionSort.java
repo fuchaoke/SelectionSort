d6b5c3a508695ec6dcab3d529c44d5e5dd0e018f
fuchaoke <36043009+fuchaoke@users.noreply.github.com>
2018-02-27 15:04:48 +0800

    增加注释

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,12 +6,12 @@ public class SelectionSort {
             System.out.print(nums+" ");
         }
         for(int i=0;i<arr.length-1;i++){
-            int k=i;
-            for(int j=k+1;j<arr.length-1;j++){
-                if(arr[j]<arr[k])
-                    k=j;
+            int k=i;//将当前下标定义为最小值下标
+            for(int j=k+1;j<arr.length-1;j++){//循环之后的数据
+                if(arr[j]<arr[k])//如果有小于当前最小值的关键字
+                    k=j;//将此关键字的下标赋值给k
             }
-        if(i!=k){
+        if(i!=k){//如果最小值下标不等于i，说明找到最小值，交换
                 int temp=arr[i];
                 arr[i]=arr[k];
                 arr[k]=temp;
